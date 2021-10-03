3846b94854238b8b2b1f814e9727c3c2f1a9c03c
Alexandru <all-kretsu@yandex.ru>
2021-10-03 17:05:07 +0300

    need to fix problem with round of defender['health']

--- Function 4/player_game_lesson_4.py
+++ Function 4/player_game_lesson_4.py
@@ -1,35 +1,33 @@
 player_1={
-	'player name' : input('Insert player 1 name: '),
-	'health' : 100,
-	'damage' : 10,
+    'player name' : input('Insert player 1 name: '),
+    'health' : 100,
+    'damage' : 10,
     'armor' : 50
 }
 
 player_2={
-	'player name' : input('Insert player 2 name: '),
-	'health' : 100,
-	'damage' : 10,
+    'player name' : input('Insert player 2 name: '),
+    'health' : 100,
+    'damage' : 10,
     'armor' : 50
 }
 
-def attack(attacker, defender):	
+def attack(attacker, defender):
 
-	while defender['health'] != 0:
+    while defender == True:
 
-		if attacker['damage'] == 10:
+        if attacker['damage'] == 10:
+            
+            defender['health'] -= round((10 + (defender['armor'] / 
+                                              defender['health']))) 
+            defender['armor'] -= 5 
+            print('{} health - {} '.format(defender['player name'],
+                                        defender['health']))
 
-			defender['health'] -= (10 + (defender['armor'] / 
-                                        defender['health'])) 
-            defender['armor'] -= 5
-
-			print('{} health - {} '.format(player_2['player name'],
-										   player_2['health']))
-			if defender['health'] == 0:
-				print(attacker['player name'] + ' win!')
-				break
-			continue
-
-
+        elif defender['health'] == 0:
+            print(attacker['player name'] + ' win!')
+            break
+    
 print(player_1['player name'] + ' vs ' + player_2['player name'])
 
 attack(player_1, player_2)
