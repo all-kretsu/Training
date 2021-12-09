b6f388d0785b337cb2ce191bf349db7c9a9d1cb8
Alexandru <all-kretsu@yandex.ru>
2021-12-09 21:23:21 +0300

    Change list function
    Added function which change a list

--- _book_LearningPython/_01_using_list_func.py
+++ _book_LearningPython/_01_using_list_func.py
@@ -1,5 +1,4 @@
 # 01 Using list in function
-
 def user_list(names):
     for name in names:
         msg = f'Hello, {name.title()}!'
@@ -7,4 +6,23 @@ def user_list(names):
 
 usernames = ['Ivan', 'Hannah']
 user_list(usernames)
+
+# 02 Change list in function
+
+def print_models(unprinted_designs, completed_models):
+    while unprinted_designs:
+        curent_models = unprinted_designs.pop()
+        print(f'\nPrint a {curent_models}')
+        completed_models.append(curent_models)
+
+def show_models():
+    print('\nThe following models have been printed!')
+    for model in completed_models:
+        print(model)
+
+unprinted_designs = ['robot', 'phone', 'dynosaur']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_models()
 
