b798397e67ab6b2b4fcbf0eb5622020c4c4399f0
all-kretsu <84284416+all-kretsu@users.noreply.github.com>
2021-12-15 11:00:29 +0300

    Update _02_OOP_Classes.py
    Added example with car

--- _book_LearningPython/_02_OOP_Classes.py
+++ _book_LearningPython/_02_OOP_Classes.py
@@ -17,6 +17,14 @@ my_dog = Dog('willie', 6)
 
 print(f'My dogs name is {my_dog.name}, he is {my_dog.age} years old')
 
+# creating class of cars
+class Car():
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+    def speed(self):
+        print(f'The name of car is {self.name}, the speed of it is {self.speed}')
+     
 
 
 
