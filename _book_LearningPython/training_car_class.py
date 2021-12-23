a68d9ddd6f21d788b96ac185452652a241b38b8a
Alexandru <all-kretsu@yandex.ru>
2021-12-23 22:40:54 +0300

    Update file with cars (added new funtions)

--- _book_LearningPython/training_car_class.py
+++ _book_LearningPython/training_car_class.py
@@ -6,7 +6,7 @@ class Car():
         self.manufacturer = manufacturer
         self.model = model
         self.year = year
-        self.odometer = 0
+        self.odometer = 6
 
     def get_descriptive_name(self):
         long_name = f'{self.year} {self.manufacturer} {self.model}'
@@ -14,16 +14,18 @@ class Car():
 
     def read_odometer(self):
         print(f'This car has {self.odometer} kilometers on it.')
-    
+
     def update_odometer(self, mileage):
-        self.odometer = mileage
-        if mileage >= :
-            
-            
-        
+        if mileage >= self.odometer:
+            self.odometer = mileage
+        else:
+            print('You cant roll back an odometer!')
+    def increment_odometer(self, kilometers):
+        self.odometer += kilometers
 
 my_new_car = Car('audi', 'a4', 2019)
 print(my_new_car.get_descriptive_name())
-my_new_car.update_odometer(60)
+my_new_car.update_odometer(100)
 my_new_car.read_odometer()
-
+my_new_car.increment_odometer(100)
+my_new_car.read_odometer()
