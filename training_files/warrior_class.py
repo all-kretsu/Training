db0c193419d717c7370c514f162254fae2311892
Alexandru <all-kretsu@yandex.ru>
2021-12-26 18:18:22 +0300

    Update warrior_class.py
    Changed game

--- training_files/warrior_class.py
+++ training_files/warrior_class.py
@@ -9,27 +9,33 @@ class Warrior():
         self.health = health
         self.turn = turn
 
-
 # create example class
-unite_1 = Warrior(0)
-unite_2 = Warrior(1)
+unite_1 = Warrior(0, 100)
+unite_2 = Warrior(1, 100)
 
-# 
-while True:
+while unite_1.health or unite_2.health <= 0:
     player_attack = random.randint(0,1)
     if player_attack == unite_1.turn:
-            unite_2.health -= unite_1.attack / 10
-            print('\nUnite 2:\n')
-            print(unite_2.__dict__)
-            if unite_2.health == 0.0:
-                break
+        print('\nUnite 1 attack:')
+        if random.randint(0, 5) == 1:
+            print('\tSuper Hit!')
+            unite_2.health -= 20
+        unite_2.health -= unite_1.attack / 10
+        print('\nUnite 2:')
+        print(unite_2.health)
+        if unite_2.health == 0.0:
+            break
     elif player_attack == unite_2.turn:
-            unite_1.health -= unite_2.attack / 10
-            print('\nUnite 1:\n')
-            print(unite_1.__dict__)
-            if unite_1.health == 0.0:
-                break
-    else:
-        pass
-print(('Unite 1 win!') if unite_1.health > 0 
-        else print('\nUnite 2 win!'))
+        print('\nUnite 2 attack:')
+        if random.randint(0, 5) == 1:
+                print('\tSuper Hit!')
+                unite_1.health -= 20
+        unite_1.health -= unite_2.attack / 10
+        print('\nUnite 1:')
+        print(unite_1.health)
+        if unite_1.health == 0.0:
+            break
+if unite_1.health <= 0:
+    print('\nUnite 2 win!')
+elif unite_2.health <= 0:
+    print('\nUnite 1 win!')
