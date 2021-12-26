1eacabe8979500ab0aa45a4fc439a9ea22823924
Alexandru <all-kretsu@yandex.ru>
2021-12-26 19:54:12 +0300

    Uptade file. Fixed bugs and problems with PEP-8

--- _book_LearningPython/dz_restaurants_classes.py
+++ _book_LearningPython/dz_restaurants_classes.py
@@ -1,22 +1,23 @@
 # home lesson
 
 
-# 01 reastaurants classes
-class Reastaurants():
-	def __init__(self, rest_name, cuisine_type):
-		# rest mean - restaurant
-		self.rest_name = rest_name
-		self.cuisine_type = cuisine_type
+# exercise 01 Restaurant classes
+class Restaurant:
+    def __init__(self, rest_name, cuisine_type):
+        # rest mean - restaurant
+        self.rest_name = rest_name
+        self.cuisine_type = cuisine_type
 
-	def describe_rest(self):
-		print(f'Reastaurant {self.rest_name} have {self.cuisine_type}.')
+    def describe_rest(self):
+        print(f'Restaurant {self.rest_name} have {self.cuisine_type}.')
 
-	def open_rest(self):
-		print(f'Reastaurant {self.rest_name} is open.')
+    def open_rest(self):
+        print(f'Restaurant {self.rest_name} is open.')
 
-chinese_rest = Reastaurants('Red Dragon', 'chinese cuisine')
-italian_rest = Reastaurants('Napoli', 'italian cuisine')
-mexican_rest = Reastaurants('Amigos', 'mexican cuisine')
+
+chinese_rest = Restaurant('Red Dragon', 'chinese cuisine')
+italian_rest = Restaurant('Napoli', 'italian cuisine')
+mexican_rest = Restaurant('Amigos', 'mexican cuisine')
 
 # print(chinese_rest.rest_name)
 chinese_rest.describe_rest()
@@ -25,17 +26,19 @@ chinese_rest.open_rest()
 italian_rest.describe_rest()
 italian_rest.open_rest()
 
-# 02 User class
-class Users():
-	def __init__(self, first_name, last_name):
-		self.first_name = first_name
-		self.last_name = last_name
 
-	def describe_user(self):
-		print(f'The user name is {self.first_name} {self.last_name}!')
+# exercise 02 User class
+class Users:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
 
-	def greet_user(self):
-		print(f'Hello {self.first_name} {self.last_name}!')
+    def describe_user(self):
+        print(f'The user name is {self.first_name} {self.last_name}!')
+
+    def greet_user(self):
+        print(f'Hello {self.first_name} {self.last_name}!')
+
 
 user_1 = Users('Alex', 'Cretu')
 user_2 = Users('Jane', 'Cretu')
