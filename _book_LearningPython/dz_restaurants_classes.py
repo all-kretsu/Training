be05cf5f7e3e6ed72a40d94a9fc66d8d081d884d
Alexandru <all-kretsu@yandex.ru>
2021-12-27 21:55:19 +0300

    Update file. Fixed bugs and problems with PEP-8

--- _book_LearningPython/dz_restaurants_classes.py
+++ _book_LearningPython/dz_restaurants_classes.py
@@ -7,6 +7,8 @@ class Restaurant:
         # rest mean - restaurant
         self.rest_name = rest_name
         self.cuisine_type = cuisine_type
+        self.new_guest = None
+        self.guests = None
 
     def describe_rest(self):
         print(f'Restaurant {self.rest_name} have {self.cuisine_type}.')
@@ -14,37 +16,22 @@ class Restaurant:
     def open_rest(self):
         print(f'Restaurant {self.rest_name} is open.')
 
+    def set_number_served(self, guests):
+        self.guests = guests
+        print(f'Number of served persons: {guests}.')
+
+    def increment_number_served(self, new_guests):
+        self.new_guest = new_guests
+        self.guests += self.new_guest
+        print(f'Number of served persons: {self.guests}.')
+
 
 chinese_rest = Restaurant('Red Dragon', 'chinese cuisine')
 italian_rest = Restaurant('Napoli', 'italian cuisine')
 mexican_rest = Restaurant('Amigos', 'mexican cuisine')
-
-# print(chinese_rest.rest_name)
 chinese_rest.describe_rest()
 chinese_rest.open_rest()
-
 italian_rest.describe_rest()
 italian_rest.open_rest()
-
-
-# exercise 02 User class
-class Users:
-    def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
-
-    def describe_user(self):
-        print(f'The user name is {self.first_name} {self.last_name}!')
-
-    def greet_user(self):
-        print(f'Hello {self.first_name} {self.last_name}!')
-
-
-user_1 = Users('Alex', 'Cretu')
-user_2 = Users('Jane', 'Cretu')
-
-user_1.describe_user()
-user_1.greet_user()
-
-user_2.describe_user()
-user_2.greet_user()
+chinese_rest.set_number_served(5)
+chinese_rest.increment_number_served(8)
